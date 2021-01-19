7129bb4fd6fe32cf20cac001fb4e4d037d3e1737
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-01-19 23:43:08 +0200

    дззззззззззззззззззззз

--- taks-1.js
+++ taks-1.js
@@ -1,33 +1,15 @@
-const categotyList = document.querySelector('ul#categories').children;
+const categotyList = [...document.querySelector('ul#categories').children];
 console.log(`В списке ${categotyList.length} категории.`);
 
-const titles = document.querySelectorAll('h2');
-const elements = document.querySelectorAll('.item>ul');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < titles.length; i+=1) {
-//     const title = titles[i].textContent;
-// };
-
-// for (let i = 0; i < elements.length; i += 1){
-//     const element = elements[i].children.length;
-// }
-
-// console.log(titles[0].textContent)
-// console.log(elements[0].children.length)
-// console.log(`Категория: ${titles[0].textContent}\nКоличество элементов: ${elements[0].children.length}`);
-// console.log(`Категория: ${titles[1].textContent}\nКоличество элементов: ${elements[1].children.length}`);
-// console.log(`Категория: ${titles[2].textContent}\nКоличество элементов: ${elements[2].children.length}`);
+categotyList.map(item => {
+    const title = item.querySelector('h2');
+    const list = item.querySelectorAll('ul li');
+    return {
+        title: title.textContent,
+        list: list.length
+    }
+}).forEach(entry => {
+    console.log('Категория: ', entry.title, '\nКоличество элементов: ', entry.list);
+});
 
