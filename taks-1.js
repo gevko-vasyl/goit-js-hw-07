2de3169aee57df0cee82a4ccd5df68ac290aafb0
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-01-19 23:11:31 +0200

    8 дз

--- taks-1.js
+++ taks-1.js
@@ -4,6 +4,19 @@ console.log(`В списке ${categotyList.length} категории.`);
 const titles = document.querySelectorAll('h2');
 const elements = document.querySelectorAll('.item>ul');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // for (let i = 0; i < titles.length; i+=1) {
 //     const title = titles[i].textContent;
 // };
@@ -14,7 +27,7 @@ const elements = document.querySelectorAll('.item>ul');
 
 // console.log(titles[0].textContent)
 // console.log(elements[0].children.length)
-console.log(`Категория: ${titles[0].textContent}\nКоличество элементов: ${elements[0].children.length}`);
-console.log(`Категория: ${titles[1].textContent}\nКоличество элементов: ${elements[1].children.length}`);
-console.log(`Категория: ${titles[2].textContent}\nКоличество элементов: ${elements[2].children.length}`);
+// console.log(`Категория: ${titles[0].textContent}\nКоличество элементов: ${elements[0].children.length}`);
+// console.log(`Категория: ${titles[1].textContent}\nКоличество элементов: ${elements[1].children.length}`);
+// console.log(`Категория: ${titles[2].textContent}\nКоличество элементов: ${elements[2].children.length}`);
 
--- task-8.js
+++ task-8.js
@@ -2,29 +2,48 @@ const numberRef = document.querySelector('input');
 const renderRef = document.querySelector('button[data-action="render"]');
 const destroyRef = document.querySelector('button[data-action="destroy"]');
 const boxesRef = document.querySelector('#boxes');
-let amount;
+let defaultSize = 30;
 
-number.addEventListener('input', valueListener)
-render.addEventListener('click', createBoxes)
-destroy.addEventListener('click', destroyBoxes)
+renderRef.addEventListener('click', valueListener)
+destroyRef.addEventListener('click', destroyBoxes)
 
 function valueListener() {
-  amount = number.target.value  
+    const amount = Number(numberRef.value)
+    createBoxes(amount);
 }
 
 function destroyBoxes() {
+    let amount = Number(numberRef.value);
     amount = 0;
+
+    while (boxesRef.firstChild) {
+        boxesRef.removeChild(boxesRef.firstChild);
+    }
+    numberRef.value = 0;
 };
 
 function createBoxes(amount) {
+    const boxes = [];
     for (let i = 0; i < amount; i += 1){
-        let width = '30px';
-        let height = '30px'
-        let boxes = [];
         const box = document.createElement('div');
+
+        let width = defaultSize + 10 * i;
+        box.style.width = `${width}px`;
+        let height = defaultSize + 10 * i;
+        box.style.height = `${height}px`;
+        
+        function random_rgba() {
+        const o = Math.round, r = Math.random, s = 255;
+        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+        }
+        const color = random_rgba();
+        box.style.backgroundColor = color;
+
         boxes.push(box);
-        boxesRef.appendChild(...boxes);
-        box.setAttribute
+        
     }
+
+    boxesRef.append(...boxes);
+    console.log(boxes);
 }
 
