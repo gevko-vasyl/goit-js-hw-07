4c857129d7c5576caad8a697d162e70cf1bd5aa9
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-01-24 21:50:08 +0200

    TASK-1

--- taks-1.js
+++ taks-1.js
@@ -2,14 +2,9 @@ const categotyList = [...document.querySelector('ul#categories').children];
 console.log(`В списке ${categotyList.length} категории.`);
 
 
-categotyList.map(item => {
-    const title = item.querySelector('h2');
-    const list = item.querySelectorAll('ul li');
-    return {
-        title: title.textContent,
-        list: list.length
-    }
-}).forEach(entry => {
-    console.log('Категория: ', entry.title, '\nКоличество элементов: ', entry.list);
+categotyList.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const list = item.querySelectorAll('ul li').length;
+    console.log('Категория: ', title, '\nКоличество элементов: ', list);
 });
 
